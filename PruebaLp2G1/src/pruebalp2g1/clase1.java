798131b251bd20b7c72a986cc08d44270a867a46
alumno04 <alumno04@TPP-LAB04-08.TPP.UPEU.EDU.PE>
2015-03-18 10:32:57 -0500

    se creó la clase1 y se loe puso atributos.

---
+++ PruebaLp2G1/src/pruebalp2g1/clase1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebalp2g1;
+
+/**
+ *
+ * @author alumno04
+ */
+public class clase1 {
+    
+    String nombre;
+    int edad;
+    int dni;
+}
